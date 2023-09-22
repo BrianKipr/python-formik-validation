390b1a775474bed6a54ab421015668a0d3c0ef2f
Briankipr <Brayos718@gmail.com>
2023-09-22 21:43:47 +0300

    assignment done

--- server/app.py
+++ server/app.py
@@ -1,30 +1,34 @@
-from distutils.log import debug
-from flask import Flask, request, jsonify, make_response
-from flask_migrate import Migrate
-from models  import Customer, db
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
 
 app = Flask(__name__)
 
+# Configuration for the SQLite database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-migrate = Migrate(app, db)
 
-db.init_app(app)
+db = SQLAlchemy(app)
 
-@app.route("/customers", methods=['GET', 'POST'])
-def customers():
-    if request.method == 'GET':
-        print(([customer.to_dict() for customer in Customer.query.all()]))
-        return make_response(jsonify([customer.to_dict() for customer in Customer.query.all()]))
+# Define the Customer model with SerializerMixin for JSON serialization
+class Customer(db.Model, SerializerMixin):
+    __tablename__ = 'customers'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    age = db.Column(db.Integer)
+    email = db.Column(db.String, unique=True)
 
-    if request.method == 'POST':
-        data = request.get_json()
-        customer = Customer(name=data.get('name'), email=data.get('email'), age=data.get('age'))
-        db.session.add(customer)
-        db.session.commit()
-        return make_response(
-            jsonify(
-                {'id': customer.id, 'name': customer.name, 'email': customer.email, 'age': customer.age }))
+    def __repr__(self):
+        return f'Customer: {self.name}, age: {self.age}, email: {self.email}'
 
-if __name__ == "__main__":
-    app.run(port="5555", debug=True)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/customers')
+def customer_list():
+    customers = Customer.query.all()
+    return render_template('customer_list.html', customers=customers)
+
+if __name__ == '__main__':
+    app.run(debug=True)
